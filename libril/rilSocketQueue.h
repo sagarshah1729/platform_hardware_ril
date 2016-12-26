c3349cb6867873fdc180fd5f400fae6fde1ce5d3
Sagar Shah <sagarshah2326@gmail.com>
2016-12-26 16:28:06 +0000

    Fix Path

--- libril/rilSocketQueue.h
+++ libril/rilSocketQueue.h
@@ -10,13 +10,13 @@
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
+* See the License for -cafthe specific language governing permissions and
 * limitations under the License.
 */
 
 #include "pb_decode.h"
 #include <pthread.h>
-#include <hardware/ril/librilutils/proto/sap-api.pb.h>
+#include <hardware/ril-caf/librilutils/proto/sap-api.pb.h>
 #include <utils/Log.h>
 
 using namespace std;
